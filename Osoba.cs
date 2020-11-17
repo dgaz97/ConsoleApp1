58908c8eb65b97ce8457a4f78f41cfb44846259f
dgaz97 <45607502+dgaz97@users.noreply.github.com>
2020-11-17 20:46:48 +0100

    Add project files.

---
+++ Osoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Osoba : IComparable
+    {
+        public Osoba(String ime, String prezime, Int16 dob)
+        {
+            Ime = ime;
+            Prezime = Prezime;
+            Dob = dob;
+        }
+        public String Ime { get; set; }
+        public String Prezime { get; set; }
+        public Int16 Dob { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Osoba o = (Osoba)obj;
+            //return this.Dob.CompareTo(o.Dob); //manje->vise
+            return o.Dob.CompareTo(this.Dob); //vise->manje
+        }
+
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime} --- {Dob} godine";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Osoba> osobe = new List<Osoba>();
+
+            osobe.Add(new Osoba("Ana","Anić",23));
+            osobe.Add(new Osoba("Ivo","Ivić",27));
+            osobe.Add(new Osoba("Pero","Perić",25));
+            osobe.Add(new Osoba("Eva","Ević",22));
+
+            osobe.ForEach(x => Console.WriteLine(x));
+
+            osobe.Sort();
+
+            osobe.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
